c8c49816cbe01ceebece0d06165d99c349caa9a9
CG-Tespy <gbcabreracgt@gmail.com>
2022-09-19 17:54:24 -0800

    Spanish translation, TS definition update

--- @typeDefs/CGT_LetterCountWrap_MV.d.ts
+++ @typeDefs/CGT_LetterCountWrap_MV.d.ts
@@ -2,6 +2,20 @@ declare namespace CGT
 {
     namespace LeCoWr
     {
+        class LCLineWrapper extends CGT.WWCore.LineWrapper {}
+
+        class LCOverflowFinder extends CGT.WWCore.Overflow.OverflowFinder 
+        {
+            protected RegularWrapSpace(args: WWCore.Overflow.IOverflowFindArgs): number;
+            protected FullWrapSpace(args: WWCore.Overflow.IOverflowFindArgs): number;
+        }
+
+        class LCTextMeasurer extends CGT.WWCore.Overflow.TextMeasurer
+        {
+            protected GetDefaultWidthOf(text: string, textField: Bitmap): number;
+        }
+
+        class LCUnderflowCascader extends CGT.WWCore.UnderflowCascader {}
 
         class WrapParams
         {
--- CGT_LetterCountWrap_MV_Main.ts
+++ CGT_LetterCountWrap_MV_Main.ts
@@ -7,18 +7,31 @@
 * Requires version 3.01.01+ of the CGT WordWrapCore plugin. To have this 
 * set as the wrapper right away, set the WordWrapCore's Wrapper param to 
 * "letterCount".
-*
-* Recommended WordWrapCore settings changes for this plugin 
-* (tested with the Amaranth font):
-* SidePadding: 1
-* MugshotWidth: 10
-* MugshotPadding: 1
 * 
 * @param LetterCountMax
 * @type number
 * @min 1
 * @default 70
 * @desc The max amount of letters you want to allow per line.
+* 
+*/
+
+/*:es
+* @plugindesc Wraps text on a letter-count-per-line basis.
+* @author CG-Tespy – https://github.com/CG-Tespy
+* @help Este es la versión 3.01.01 de este plugin. Lo probé con versiones RMMV 
+* 1.5.1 y 1.6.2. 
+* 
+* Necesita versión 3.01.01+ del CGT WordWrapCore (Núcleo de Ajustelíneas) 
+* plugin. Para poner este como el ajustelíneas activo inmediatamente, pon el
+* Ajustelíneas parám del WordWrapCore como "letterCount".
+* 
+* @param LetterCountMax
+* @text MáxCantidadDeLetras
+* @type number
+* @min 1
+* @default 70
+* @desc La cantidad máximo de letras que quieres permitir por línea.
 * 
 */
 
